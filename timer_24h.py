ddb9a31bc17f3a45ca4d44a43902daf4e05e767d
Dourte Simon <simon.dourte@student.unamur.be>
2023-12-05 00:45:24 +0100

    ajout du background

--- timer_24h.py
+++ timer_24h.py
@@ -1,11 +1,17 @@
 from tkinter import *
 import tkinter as tk
 from datetime import datetime, timedelta
+from PIL import Image, ImageTk
 
 class ChronometerApp:
     def __init__(self, master):
         self.master = master
         master.title("Chronomètre")
+
+        self.image_path = "images/Affiche VIva pour chrono.png"
+        self.load_background_image()
+
+        self.master.bind('<Configure>', self._resize_image)
 
         self.max_duration = timedelta(hours=24)
         self.start_time = datetime.now()
@@ -14,32 +20,45 @@ class ChronometerApp:
 
         self.create_widgets()
 
+    def load_background_image(self):
+        self.image = Image.open(self.image_path)
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background_label = Label(self.master, image=self.background_image)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    def _resize_image(self, event):
+        if event.widget is self.master:
+            # resize background image to fit the frame size
+            image = self.image.resize((event.width, event.height))
+            self.background_image = ImageTk.PhotoImage(image)
+            self.background_label.configure(image=self.background_image)
+
     def create_widgets(self):
-        input_wanted = input("Voulez vous choisir le temps (oui/non) ? ")
+        input_wanted = input("Voulez-vous choisir le temps (oui/non) ? ")
         if input_wanted == "oui":
-            self.input_timer()  # Appel correct de la méthode
+            self.input_timer()
         else:
             self.max_duration = timedelta(hours=24)
 
         title = tk.Label(self.master, text="Chronomètre", font=('Helvetica', 20))
-        title.grid(row=0, column=0, columnspan=2, pady=10)
+        title.pack()
 
         self.timer_label = tk.Label(self.master, font=('Helvetica', 150))
-        self.timer_label.grid(row=1, column=0, columnspan=2, pady=20)
+        self.timer_label.pack()
 
         self.update_timer_display()
 
         start_button = tk.Button(self.master, text="Start", command=self.start_timer)
-        start_button.grid(row=2, column=0, pady=10)
+        start_button.pack(pady=10)
 
         stop_button = tk.Button(self.master, text="Stop", command=self.stop_timer)
-        stop_button.grid(row=2, column=1, pady=10)
-        
+        stop_button.pack(pady=10)
+
         reset_button = tk.Button(self.master, text="Reset", command=self.reset_timer)
-        reset_button.grid(row=3, column=0, pady=10)
-        
+        reset_button.pack(pady=10)
+
         reprendre_button = tk.Button(self.master, text="Reprendre", command=self.reprendre_timer)
-        reprendre_button.grid(row=3, column=1, pady=10)
+        reprendre_button.pack(pady=10)
 
     def update_timer_display(self):
         elapsed_time = datetime.now() - self.start_time
@@ -67,26 +86,27 @@ class ChronometerApp:
         if self.running:
             self.running = False
             self.update_timer_display()
-            
+
     def reset_timer(self):
         if self.start_time != datetime.now():
             self.start_time = datetime.now()
             self.running = False
             self.update_timer_display()
-            
+
     def reprendre_timer(self):
         if not self.running:
             elapsed_pause_time = datetime.now() - self.start_time
             self.start_time = datetime.now() - elapsed_pause_time
             self.running = True
             self.update_timer_display()
-            
+
     def input_timer(self):
         input_time = input("Entrez le nombre de temps à chronométrer (xx:xx:xx): ")
         input_time = input_time.split(":")
         self.max_duration = timedelta(hours=int(input_time[0]), minutes=int(input_time[1]), seconds=int(input_time[2]))
-        
 
-root = tk.Tk()
-app = ChronometerApp(root)
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("960x540")
+    app = ChronometerApp(root)
+    root.mainloop()
